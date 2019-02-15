e78ee52f5ece6a09801abeffe4ff854bff89b397
wangjie <15236730688@163.com>
2019-02-15 18:21:15 +0800

    fastdfs客户端测试

---
+++ taotao-content/taotao-content-service/src/test/java/com/wj/taotao/test/FastdfsClientDemo.java
@@ -0,0 +1,20 @@
+package com.wj.taotao.test;
+
+import org.csource.fastdfs.*;
+
+public class FastdfsClientDemo {
+
+    public static void main(String[] args) throws Exception{
+        ClientGlobal.init("D:\\wk\\idea\\taotao\\taotao-content\\taotao-content-service\\src\\main\\resources\\fdfs_client.conf");
+        TrackerClient trackerClient = new TrackerClient();
+        TrackerServer trackerServer = trackerClient.getConnection();
+        StorageServer storageServer = null;
+        StorageClient storageClient = new StorageClient(trackerServer,storageServer);
+
+        String[] result = storageClient.upload_file("D:\\wk\\idea\\taotao\\taotao-content\\taotao-content-service\\src\\test\\java\\com\\wj\\taotao\\test\\123.jpg", "jpg", null);
+        for(String str : result){
+            System.out.println(str);
+        }
+
+    }
+}
